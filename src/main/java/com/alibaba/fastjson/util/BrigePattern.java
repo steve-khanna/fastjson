90012bdbc1371db636562ef0e35437d935ed7126
Steve Khanna <steve.khanna@ucalgary.ca>
2021-04-16 21:32:22 -0600

    Added updated README

--- src/main/java/com/alibaba/fastjson/util/BrigePattern.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.fastjson.util;
-
-public abstract class BrigePattern {
-
-    static BridgeStruct step3(int e2, long mv, long mp, long mm, boolean even, int mmShift) {
-        return null;
-    }
-}
